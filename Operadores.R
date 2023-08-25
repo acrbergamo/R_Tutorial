ffdfac7145a5e72159a4fa9347abfa29c3bb6304
Adriano Rocha <adrianocrbergamo@outlook.com>
2023-08-25 01:10:13 -0300

    Start

---
+++ Operadores.R
@@ -0,0 +1,61 @@
+# OBJETIVO: realizar operacoes matematicas, logicas e de comparacao
+
+# COMO
+# Utilizando recursos basicos
+
+# Soma
+4 + 2
+
+# Subtracao
+16 - 8
+
+# Divisao
+4/2
+
+# Multiplicacao
+4 * 2
+
+# Potencia
+4 ** 2
+4 ^ 2
+
+# Igual?
+4 == 2
+4 == 4
+
+# Diferente?
+4 != 2
+4 != 4
+
+# Maior?
+4 > 2
+4 > 8
+
+# Menor
+4 < 2
+4 < 8
+
+# Maior ou Igual?
+4 >= 2
+4 >= 4
+
+# Menor ou Igual?
+4 <= 2
+4 <= 4
+
+# E
+4 == 4
+4 == 4 & 8 == 16 
+4 == 4 & 8 != 16
+4 > 2 & 8 < 16
+
+# OU
+4 == 4
+8 == 16
+4 == 8 | 8 == 16
+4 == 8 | 8 != 16
+4 > 2 | 8 > 16
+
+# Negacao
+!4 == 4
+!4 < 2
---
+++ TidosDeDados.R
@@ -0,0 +1,112 @@
+# OBJETIVO: entender como a linguagem R organiza os dados
+
+# COMO?
+# Exemplo
+
+# Armazenamento numerico
+
+Salario <- 3450.89
+Horas <- 220
+
+SH <- Salario/Horas
+SHi <- as.integer(Salario/Horas)
+SHr <- round(Salario/Horas)
+
+
+Numeros1 <- Salario + Horas 
+Numeros2 <- c(Salario, Horas)
+
+class(SH)
+class(SHi)
+class(SHr)
+class(Numeros1)
+class(Numeros2)
+
+# Armazenamento de caracteres
+
+Nome_1 <- "Eduardo Abreu"
+Nome_2 <- "Amanda Lopes"
+Idade <- "25"
+
+Idade + 5
+
+Nomes <- Nome_1 + Nome_2
+Nomes <- c(Nome_1, Nome_2)
+
+Nomes
+Nomes[1]
+Nomes[2]
+
+Nome_1 != Nome_2
+
+class(Nomes)
+class(Idade)
+summary(Idade)
+
+# Armazenamento de fatores
+
+CargaHoraria <- c(220, 220, 150, 100, 100)
+summary(CargaHoraria)
+
+CargaHoraria <- as.factor(CargaHoraria)
+summary(CargaHoraria)
+mode(CargaHoraria)
+class(CargaHoraria)
+
+# Armazenamento logico
+
+L1 <- Salario > Horas
+L1
+
+L2 <- Salario < Horas
+L2
+
+Logico <- TRUE
+Logico <- "TRUE"
+Logico2 <- c(1, FALSE, 3)
+
+# Vetores - Estrutura básica de dados
+# Uma sequencia de dados do mesmo tipo
+
+# vetor de caracteres
+is.vector(Nomes)
+mode(Nomes)
+
+# vetor numerico
+is.vector(Horas)
+mode(Horas)
+
+# vetor logico
+is.vector(L1)
+mode(L1)
+
+# Lista - vetor com tipos de dados diferentes
+a <- c(1, 2, 3, 4, 5)
+b <- c(1, "2", 3, 4, 5)
+a
+b
+b <- as.numeric(b)
+b
+
+is.list(a)
+is.list(b)
+
+is.vector(a)
+is.vector(b)
+
+
+b <- list(10, "2", 8)
+is.list(b)
+mode(b)
+str(b)
+
+e <- list(c(10, 6, 51, 5), "2", 8)
+is.list(e)
+mode(e)
+str(e)
+e[[1]][2]
+
+# Matrizes - Duas dimensoes um tipo de dado
+
+
+
